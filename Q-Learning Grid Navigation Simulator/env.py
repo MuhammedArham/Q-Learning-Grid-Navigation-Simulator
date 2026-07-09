29beec0e8276d6b8553094fa0040c0b203650c9a
muhammedArham <muhammedarham@gmail.com>
2026-07-10 05:50:33 +1000

    Add env implementation

--- Q-Learning Grid Navigation Simulator/env.py
+++ Q-Learning Grid Navigation Simulator/env.py
@@ -3,3 +3,13 @@ import time
 
 import numpy as np
 
+
+# Class representing a static grid environment
+class StaticGridEnv:
+    def __init__(self, seed=None):
+        """
+        Initialize the static grid environment.
+
+        Args:
+            seed (int): Optional random seed for reproducibility.
+        """
